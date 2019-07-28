7d21e93939d892588a45218362c31bcfc1c26c2b
mrmaninew <manikanta@live.in>
2019-07-28 23:24:47 +0530

    order summary

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,14 +1,43 @@
 import React, { Component } from "react";
+import axios from "../../axios-orders";
 import Order from "../../components/Order/Order";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true
+  };
+  componentDidMount() {
+    axios
+      .get("/orders.json")
+      .then(response => {
+        const fetchedOrder = [];
+        for (let key in response.data) {
+          fetchedOrder.push({
+            ...response.data[key],
+            id: key
+          });
+        }
+        this.setState({ loading: false, orders: fetchedOrder });
+      })
+      .catch(err => this.setState({ loading: false }));
+  }
   render() {
     return (
       <div>
-        <Order />
+        {this.state.orders.map(order => {
+          return (
+            <Order
+              key={order.id}
+              ingredients={order.ingredients}
+              price={order.price}
+            />
+          );
+        })}
       </div>
     );
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
